1548f3c1519f785e2bb9e1e338ad0c3fdd463d93
Estefany Botina <ebotinaperez@gmail.com>
2025-09-10 20:24:54 -0500

    se sube el footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
 import './globals.css'
 import Navbar from './components/NavBar/Navbar'
+import Footer from './components/footer/Footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body className="min-h-screen flex flex-col">
-        {/* Navbar global, se renderiza una sola vez */}
+        {/* Navbar global */}
         <Navbar />
         
-        {/* Contenido de cada página */}
+        {/* Contenido dinámico */}
         <main className="flex-1 p-4">{children}</main>
 
-        {/* Footer opcional */}
-        <footer className="bg-gray-800 text-white text-center p-2">
-          © {new Date().getFullYear()} Mi Proyecto
-        </footer>
+        {/* Footer global */}
+        <Footer />
       </body>
     </html>
   )
